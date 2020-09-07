15ec91535822d0b2a5e18ef485766ebba9ae481d
Henrik Grønbech <henrikgronbech@gmail.com>
2020-09-07 14:50:04 +0200

    Update files in p001 to be consistent  (#5)
    * Add deno to settings.json
    
    * Update problems to be more consistent

--- p001/p001_comprehension.py
+++ p001/p001_comprehension.py
@@ -1,5 +1,4 @@
 # Henrik Grønbech
-
 # https://projecteuler.net/problem=1
 
 """
@@ -12,24 +11,10 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-# For loop
+# One-liner
 def sum_multiples(upper, mult0, mult1):
-    total = 0
-    for i in range(upper):
-        if i % mult0 == 0 or i % mult1 == 0:
-            total += i
-    return total
-
-
-# One-liner; List comprehension
-def sum_multiples_list_comprehension(upper, mult0, mult1):
     return sum((i for i in range(upper) if i % mult0 == 0 or i % mult1 == 0))
 
 
 if __name__ == "__main__":
-    upper = 1000
-    mult0 = 3
-    mult1 = 5
-    # assert sum_multiples(upper, mult0, mult1) == sum_multiples_list_comprehension(upper, mult0, mult1)
-    
-    print(sum_multiples(upper, mult0, mult1))
+    print(sum_multiples(1000, 3, 5))
--- p001/p001_for_loop.jl
+++ p001/p001_for_loop.jl
@@ -1,7 +1,7 @@
-% Henrik Grønbech
-% https://projecteuler.net/problem=1
+# Henrik Grønbech
+# https://projecteuler.net/problem=1
 
-%{
+#=
 Project Euler Problem 1
 =======================
 
@@ -9,18 +9,18 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5,
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
-%}
+=#
 
-function p001()
-    printf("%d\n", sum_multiples(1000, 3, 5))
-end
 
-function total = sum_multiples(upper, mult1, mult2)
-    total = 0;
-
+function sum_multiples(upper, mult1, mult2)
+    sum = 0
     for i = 1:upper-1
-        if mod(i,mult1) == 0 || mod(i,mult2) == 0
-            total = total + i;
+        if i % mult1 == 0 || i % mult2 == 0
+            sum += i
         end
     end
+    return sum
 end
+
+
+println(sum_multiples(1000, 3, 5))
