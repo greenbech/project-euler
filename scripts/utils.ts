96fa451a64c26b11adaecba31618207467362b0a
Henrik Grønbech <henrikgronbech@gmail.com>
2020-09-09 15:47:04 +0200

    📂 Update scripts and reformat source files (#8)
    * Update scripts and reformat source files
    
    * Update typos scripts/README.md
    
    * Update emojis 👌🎷🎉

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,4 +1,5 @@
 import * as path from "https://deno.land/std/path/mod.ts";
+import { sprintf } from "https://deno.land/std@0.68.0/fmt/printf.ts";
 
 interface ObjectLiteral {
   [key: string]: string;
@@ -44,7 +45,7 @@ export async function runFile(
 
   if (value !== answer) {
     console.log(
-      `Wrong answer with file ${file_name}. Is was ${
+      `❌ Wrong answer with file ${file_name}. Is was ${
         value
       } but should have been ${answer}`,
     );
@@ -56,4 +57,48 @@ export async function runFile(
     "value": value,
     "execution_time": Math.round(t1 - t0),
   };
+}
+
+export function checkFormatSourceFile(file_name: string, problem_number: number) {
+  const text = Deno.readTextFileSync(file_name)
+
+  const file_prefix = `p${sprintf("%1.3d", problem_number)}`  
+  if (!path.basename(file_name).startsWith(file_prefix)) {
+    console.log(`❌ File ${file_name} did have "${file_prefix}" as prefix`);
+    Deno.exit(1)
+  }
+
+  if (!text.includes(` Henrik Grønbech; https://projecteuler.net/problem=${problem_number}\n`)) {
+    console.log(`❌ File ${file_name} did not include " Henrik Grønbech; https://projecteuler.net/problem=${problem_number}\n"`);
+    Deno.exit(1)
+  }
+}
+
+export function checkFormatReadme(file_name: string, problem_number: number) {
+  const text = Deno.readTextFileSync(file_name)
+
+  if (!text.includes(`Problem ${problem_number}:`)) {
+    console.log(`❌ File ${file_name} did not include "Problem ${problem_number}"`);
+    Deno.exit(1)
+  }
+
+  if (!text.includes(`](https://projecteuler.net/problem=${problem_number})`)) {
+    console.log(`❌ File ${file_name} did not include "https://projecteuler.net/problem=${problem_number}"`);
+    Deno.exit(1)
+  }
+
+  if (!text.includes(`## Problem description\n`)) {
+    console.log(`❌ File ${file_name} did not include "## Problem description"`);
+    Deno.exit(1)
+  }
+
+  if (!text.includes(`## Solution\n`)) {
+    console.log(`❌ File ${file_name} did not include "## Solution"`);
+    Deno.exit(1)
+  }
+
+  if (!text.includes(`## Benchmark\n`)) {
+    console.log(`❌ File ${file_name} does not include "## Benchmark"`);
+    Deno.exit(1)
+  }
 }
