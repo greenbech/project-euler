461fe81e2ac6fe1f337b930c8a6857c4a4d8e2ab
Henrik Grønbech <henrikgronbech@gmail.com>
2020-09-11 13:31:10 +0200

    Update scripts (#11)

--- scripts/pecheck.ts
+++ scripts/pecheck.ts
@@ -1,29 +1,42 @@
-import { runFile, extensionsToLanguage, checkFormatSourceFile, checkFormatReadme } from "./utils.ts";
+import {
+  runFile,
+  extensionsToLanguage,
+  checkFormatSourceFile,
+  checkFormatReadme,
+} from "./utils.ts";
 
 import * as path from "https://deno.land/std/path/mod.ts";
 import { walkSync } from "https://deno.land/std/fs/mod.ts";
 
 // Load directory from first command line argument
-const directory: string = Deno.args[0] === "." ?  Deno.cwd() : Deno.args[0];
-const problem_number = parseInt(path.basename(directory).slice(1))
+const directory: string = Deno.args[0];
+const problem_number = parseInt(
+  path.basename(Deno.realPathSync(directory)).slice(1),
+);
 
 // Load answer from `answer.txt` file
 let answer: number = parseInt(
   Deno.readTextFileSync(path.join(directory, "answer.txt")),
 );
 
+let error = false;
 // Run files and check for correctness
 for (const entry of walkSync(directory)) {
   if (entry.isFile) {
     if (entry.name === "README.md") {
-        console.log(`Checking file README.md`)
-        checkFormatReadme(entry.path, problem_number)
+      console.log(`Checking file README.md`);
+      error = error || checkFormatReadme(entry.path, problem_number);
     } else if (path.extname(entry.name) in extensionsToLanguage) {
-        console.log(`Checking file ${entry.name}`);
-        checkFormatSourceFile(entry.path, problem_number);
-        await runFile(entry.name, directory, answer);
+      console.log(`Checking file ${entry.name}`);
+      checkFormatSourceFile(entry.path, problem_number);
+      await runFile(entry.name, directory, answer);
     }
   }
 }
 
-console.log(`✅ All checks passed`);
+if (error) {
+  console.log(`❌ Not all checks passed`);
+  Deno.exit(1);
+} else {
+  console.log(`✅ All checks passed`);
+}
